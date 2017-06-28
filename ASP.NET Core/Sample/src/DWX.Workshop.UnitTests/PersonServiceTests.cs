779c7c9d8807c2d5ebcd9a9063b4c11ac90256f9
Benjamin Abt <mail@benjamin-abt.com>
2017-06-28 15:40:38 +0200

    Added Moq

--- ASP.NET Core/Sample/src/DWX.Workshop.UnitTests/PersonServiceTests.cs
+++ ASP.NET Core/Sample/src/DWX.Workshop.UnitTests/PersonServiceTests.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using Castle.Components.DictionaryAdapter;
 using DWX.Workshop.UnitTests.Environment;
 using DWX17.Workshop.Database;
+using DWX17.Workshop.Database.Entities;
 using DWX17.Workshop.Services;
+using Moq;
 using Xunit;
 
 namespace DWX.Workshop.UnitTests
@@ -29,5 +34,33 @@ namespace DWX.Workshop.UnitTests
 
         
         }
+
+        [Fact]
+        public void Person_GetAll_Should_Return_One()
+        {
+            // Arrange
+            string name = "Ben";
+            DateTimeOffset dateOfBirth = DateTimeOffset.Parse("2017-06-29", CultureInfo.InvariantCulture, DateTimeStyles.None);
+            Mock<IPersonRepository> personRepositoryMock = new Mock<IPersonRepository>();
+            {
+                // Mock Config
+                personRepositoryMock.Setup(pr => pr.GetAll()).Returns(() => new[]{ PersonEntity.New(name, dateOfBirth) }.ToList());
+            }
+            PersonService personService = new PersonService(personRepositoryMock.Object);
+
+            // Act
+            List<Person> persons = personService.GetAll();
+            int count =persons.Count;
+            Person firstPerson = persons.First();
+
+            // Assert
+            Assert.Equal(count, 1);
+            Assert.NotEqual(null, firstPerson);
+            Assert.Equal(name, firstPerson.Name);
+            Assert.Equal(dateOfBirth, firstPerson.DateOfBirth);
+
+        }
+
+
     }
 }
