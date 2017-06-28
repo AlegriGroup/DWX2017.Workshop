141e14ccf0f3acf63c26d818a8417ee4c1268ee5
Benjamin Abt <mail@benjamin-abt.com>
2017-06-28 15:45:51 +0200

    Added Docker and CLI

---
+++ ASP.NET Core/Sample/src/DWX17.Workshop.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DWX17.Workshop.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
